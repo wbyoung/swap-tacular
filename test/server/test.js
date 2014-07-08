8a13bb8bc69fb1e891189ed243779e5332e8d093
Ariel Spear <simpleredfox@gmail.com>
2014-07-08 16:26:29 -0700

    editing test for travis

--- test/server/test.js
+++ test/server/test.js
@@ -1,5 +1,8 @@
 'use strict';
+var swapTacular = require('../server/application');
 
 describe('server', function() {
-  it('will have tests');
+  it('will have tests', function(){
+    
+  });
 });
