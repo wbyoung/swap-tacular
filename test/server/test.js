582bd29ae30f8db2577b604bc504081b99a0affa
Ariel Spear <simpleredfox@gmail.com>
2014-07-08 16:29:18 -0700

    Trying to get coverage working via Coveralls.

--- test/server/test.js
+++ test/server/test.js
@@ -1,8 +1,8 @@
 'use strict';
-var swapTacular = require('../server/application');
+var swapTacular = require('../../server/application');
 
 describe('server', function() {
   it('will have tests', function(){
-    
+
   });
 });
