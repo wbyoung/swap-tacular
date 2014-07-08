3869b20340daa73b30ac85fd8746492a00598e9e
Grant Stampfli <mail@gstampfli.com>
2014-07-08 14:48:19 -0700

    Removed bad server setup code.

--- server/config/index.js
+++ server/config/index.js
@@ -3,14 +3,5 @@
 var _ = require('lodash');
 var base = require('./env/base');
 var overrides = require('./env/' + base.env);
-var pg = require('pg');
-
-pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-  client.query('SELECT * FROM users', function(err, result) {
-    done();
-    if(err) return console.error(err);
-    console.log(result.rows);
-  });
-});
 
 module.exports = _.extend({}, base, overrides);
