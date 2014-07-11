bc76ee9555891fb6e46953a46dad715abb25b223
Grant Stampfli <mail@gstampfli.com>
2014-07-11 10:32:53 -0700

    merged tests

--- test/app/test.js
+++ test/app/test.js
@@ -1,6 +1,29 @@
 'use strict';
 var fixture = __fixture('postGET');
-
+describe('app', function() {
+  afterEach(function() {
+    App.reset();
+  });
+  
+  it.skip('will have a create post page', function() {
+    visit('/create');
+    andThen(function () {
+      expect(currentRouteName()).to.eql('create');
+    });
+  });
+  it.skip('will have a create button', function() {
+    visit('/create');
+    andThen(function () {
+      expect(find('button.create.post').length).to.eql(1);
+    });
+  });
+  it('will have an input box', function() {
+    visit('/create');
+    andThen(function () {
+      expect(find('input[type="text"].content.post').length).to.eql(1);
+    });
+  });
+});
 describe('app', function() {
 	before(function () {
 		this.server = sinon.fakeServer.create();
