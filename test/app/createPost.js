5854740ceac16f83697cb3ffdf811b938e72d2ca
Grant Stampfli <mail@gstampfli.com>
2014-07-11 10:01:44 -0700

    Fxied typo.

--- test/app/createPost.js
+++ test/app/createPost.js
@@ -5,7 +5,7 @@ describe('app', function() {
     App.reset();
   });
   
-  it('will have a creat post page', function() {
+  it('will have a create post page', function() {
     visit('/create');
     andThen(function () {
       expect(currentRouteName()).to.eql('create');
