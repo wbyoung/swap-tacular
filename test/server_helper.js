5a927f88e6a990602ce299c5145395e1e802b171
Whitney Young <wbyoung@fadingred.com>
2014-07-17 13:16:53 -0700

    Improved gulp server test setup.

--- test/server_helper.js
+++ test/server_helper.js
@@ -9,21 +9,3 @@ GLOBAL.__fixture = function(name) {
   var path = require('path');
   return _.cloneDeep(require(path.join(__dirname, 'fixtures', name)));
 };
-
-var mocha = require('mocha');
-var models = require('../server/models');
-var disconnect = function() {
-  models._kenx.client.pool.destroy();
-};
-
-// hook into mocha to figure out when all tests are finished running
-(function(run) {
-  mocha.Runner.prototype.run = function() {
-    var args = Array.prototype.slice.call(arguments);
-    var cb = args[0]; args[0] = function() {
-      cb.apply(this, arguments);
-      disconnect();
-    };
-    return run.apply(this, args);
-  };
-}(mocha.Runner.prototype.run));
