29ca20f4af1c8776898b3d6061b789e6773358c1
Grant Stampfli <mail@gstampfli.com>
2014-07-17 14:20:22 -0700

    added a btn to show text area when comment btn is pushed

--- test/app/test.js
+++ test/app/test.js
@@ -145,8 +145,23 @@ describe('app', function() {
 			expect(find('button.comment.post').text()).to.eql('Comment');
 			expect(find('button.comment.post').length).to.exist;
 		});
-		it('will create an input fiield when comment button is clicked', function(){
-			
+
+		it('has no input areas before comment buttons is clicked', function() {
+			expect(find('textarea.comment.post').length).to.eql(0);
+		});
+
+		it('will create an input area when comment btn is clicked', function(){
+			click('button.comment.post:first');
+			andThen(function() {
+				expect(find('textarea.comment.post').length).to.eql(1);
+			});
+		});
+
+		it('will hide the comment btn when clicked', function(){
+			click('button.comment.post:first');
+			andThen(function() {
+				expect(find('button.comment.post:first').length).to.eql(0);
+			});
 		});
 	});
 });
