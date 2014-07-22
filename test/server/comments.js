d83c5cb2e3963cf817d8c385fd558af9537f7f4e
Tian Song <tian.y.song@gmail.com>
2014-07-22 11:58:16 -0700

    added fixture data to commnetsGET.

--- test/server/comments.js
+++ test/server/comments.js
@@ -1,15 +1,10 @@
 'use strict';
 
-var _ = require('lodash');
 var expect = require('chai').expect;
-var util = require('util');
 var bluebird = require('bluebird'), Promise = bluebird;
-var request = require('request'),
-    requestAsync = bluebird.promisify(request, request);
 var app = require('../../server/application');
 var models = require('../../server/models');
 var port = 383273;
-var baseURL = util.format('http://localhost:%d', port);
 
 var Post = models.Post,
     Comment = models.Comment;
@@ -18,4 +13,34 @@ var fixtureHelpers = require('./helpers/fixtures'),
     createUser = fixtureHelpers.createUser,
     createUsers = fixtureHelpers.createUsers,
     createToken = fixtureHelpers.createToken,
-    createPosts = fixtureHelpers.createPosts;
+    createPosts = fixtureHelpers.createPosts,
+    requestFixture = fixtureHelpers.requestFixture;
+
+var dateRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/;
+var tokenValue = 'ff13689653e86dc1ad0f9b4a34978192a918c6d4';
+
+describe('server', function() {
+  before(function(done) { this.server = app.listen(port, function() { done(); }); });
+  after(function(done) { this.server.close(done); });
+  beforeEach(function(done) {
+    Promise.resolve() // start promise sequence
+    .then(function() {
+      return models._bookshelf.knex('comments').del();
+    })
+    .then(function() {
+      return models._bookshelf.knex('posts').del();
+    })
+    .then(function() {
+      return models._bookshelf.knex('tokens').del();
+    })
+    .then(function() {
+      return models._bookshelf.knex('users').del();
+    }).done(function() { done(); }, done);
+  });
+
+  it('will get comments', function(done) {
+    var fixture = __fixture('commentsGET');
+  
+  });
+
+});
